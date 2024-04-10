1c0c8396ef6699230d09ba22f1f6ef093495ef9a
Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2024-04-10 19:35:52 +0530

    done nodemailer services
    the application is very simple

--- services/NodeMailerServices.js
+++ services/NodeMailerServices.js
@@ -10,25 +10,67 @@ export const sendMail = (row, sheets) => {
         }
     });
 
-    // Apply the inlineBase64 plugin to the transporter
     transporter.use('compile', inlineBase64({ cidPrefix: 'somePrefix_' }));
 
-    // Extract base64 image data from sheets
     const base64Image = sheets.values[row][1].split(",")[1]; // Extracting base64 part after the comma
 
     const mailOptions = {
-        from: `<${process.env.EMAIL}>`,
+        from: `Telugu Cultural Association <${process.env.EMAIL}>`,
         to: `${sheets.values[row][0]}`,
-        subject: 'NodeMailer Testing',
+        subject: "Food Coupon of TCA's Ugadi Event",
         html: `
-            <html>
-            <head>
-                <title>NodeMailer Testing</title>
-            </head>
-            <body>
-                <img src="data:image/png;base64,${base64Image}" alt="Image">
-            </body>
-            </html>
+        <html>
+<head>
+    <title>Food Coupon</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            line-height: 1.6;
+        }
+        .container {
+            max-width: 1000px;
+            padding: 20px 0;
+        }
+        p {
+            font-size: 16px;
+            line-height: 1.6;
+            margin-bottom: 20px;
+        }
+        img {
+            display: block;
+            margin: 0 auto;
+            max-width: 100%;
+            height: auto;
+        }
+        .note {
+            font-size: 14px;
+        }
+        .qr-code {
+            width: 250px; /* Adjust the size of the QR code image as needed */
+            margin: 20px 0;
+        }
+        .warning {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <p>Dear <b>{name}</b>,</p>
+        <p>Wish you and your family a very <b>Happy Ugadi!</b></p>
+        <p>We invite you to join us at Auditorium 1, Convention Centre on 19th April to celebrate the festival of Ugadi - Telugu New Year from 7.30 PM onwards & enjoy the delicious <b>Telugu Bojanam.</b></p>
+        <img class="qr-code" src="data:image/png;base64,${base64Image}" alt="QR Code">
+        <p class="note">NOTE:<br/>
+            1. The food will be served from 7.30 pm onwards outside the Convention Centre.<br/>
+            2. Reach there with the QR Code Coupon given above to avail the food facility.<br/>
+            3. <span class="warning">The coupon is Unique & One Time Redeemable.</span> So make sure you are not sharing the link with anyone else.<br/>
+        </p>
+    </div>
+</body>
+</html>
+
         `,
     };
 
